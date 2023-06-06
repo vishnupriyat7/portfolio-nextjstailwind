5fd9321a37a79e234b4597ef9b319ea4fffb73b2
vishnupriyat7 <101249921+vishnupriyat7@users.noreply.github.com>
2023-06-06 19:25:36 +0530

    butto style changed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import html from '../assets/html.png';
+// import css from '../assets/css.png';
+// import javascript from '../assets/javascript.png';
+// import reactImage from '../assets/react.png';
+// import nextjs from '../assets/nextjs.png';
+// import github from '../assets/github.png';
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
+      <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+        <div>
+          <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+          <p className='py-6'>These are the technologies I've worked with  </p>
+        </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+          <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
+            <img src={html} alt="" className='w-20 mx-auto' />
+            <p>HTML</p>
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
