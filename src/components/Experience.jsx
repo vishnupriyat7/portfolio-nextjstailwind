4eeaf7873cc48c0ea5e9d4be3e6feff847559952
vishnupriyat7 <101249921+vishnupriyat7@users.noreply.github.com>
2023-06-07 20:55:19 +0530

    final-submit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -79,10 +79,12 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-        <div>
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+
+      
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -27,9 +27,9 @@ const NavBar = () => {
     ];
 
     return (
-        <div className="flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed">
+        <div className="flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed sm:mb-4">
             <div>
-                <h3 className="lg:text-3xl font-signature ml-2 md:text-2xl">Vishnu Priya T</h3>
+                <h3 className="lg:text-3xl font-signature ml-2 md:text-2xl sm:text-xl">Vishnu Priya T</h3>
             </div>
 
             <ul className="flex">
@@ -44,7 +44,7 @@ const NavBar = () => {
             </ul>
             <div
                 onClick={() => setNav(!nav)}
-                className="pr-4 cursor-pointer z-10 text-gray-500 md:hidden"
+                className="pr-4 cursor-pointer z-10 text-gray-500 md:hidden sm:hidden"
             >
                 {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
             </div>
