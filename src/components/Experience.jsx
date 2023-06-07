7bd11a384743eb3445254bf1554cfd3c80dea5ad
vishnupriyat7 <101249921+vishnupriyat7@users.noreply.github.com>
2023-06-07 21:25:38 +0530

    issue cleared

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -79,12 +79,10 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div className="pb-8">
-
-      
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,10 +29,10 @@ const NavBar = () => {
     return (
         <div className="flex justify-between items-center w-full h-20 text-white px-4 bg-black fixed sm:mb-4">
             <div>
-                <h3 className="lg:text-3xl font-signature ml-2 md:text-2xl sm:text-xl">Vishnu Priya T</h3>
+                <h3 className="lg:text-4xl font-signature ml-2 md:text-3xl">Vishnu Priya T</h3>
             </div>
 
-            <ul className="flex">
+            <ul className="hidden md:flex">
                 {links.map(({ id, link }) =>
                     <li
                         key={id}
